7b3181179fdf96eec5b56d1233ed5d3eccfbf4a9
Zackary Cammack <zack.cammack@gmail.com>
2020-08-29 17:42:53 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+      @word = word
+  end
+
+  def match(list_of_words)
+    list_of_words.select do |word|
+      (@word.split("").sort) == (word.split("").sort)
+    end
+  end
+end
